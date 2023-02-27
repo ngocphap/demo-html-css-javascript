654ed3daf80b41b0f8fe20954dfd4b1343ef6264
ngocphap <ngocphap5@gmail.com>
2023-02-27 23:29:52 +0700

    Dropdown Dashboard

---
+++ Dropdown sidebar/script.js
@@ -0,0 +1,13 @@
+const arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    const arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+    arrowParent.classList.toggle("showMenu");
+  });
+}
+const sidebar = document.querySelector(".sidebar");
+const sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
